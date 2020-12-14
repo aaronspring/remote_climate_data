8887c64b6cb1ba87e24f4dbf4c59118f1b3f2b56
AS <aaron.spring@mpimet.mpg.de>
2020-12-14 11:11:13 +0100

    add csv to tests

--- tests/test_remote_catalog.py
+++ tests/test_remote_catalog.py
@@ -13,16 +13,18 @@ def test_check_all_items():
     """Test all items.to_dask() except ceda requiring credentials"""
     for item_str in cat.walk(depth=3):
         print(item_str)
-        if 'CRU_TS' not in item_str: # avoids also CRU datasets requiring credentials at ceda
+        if 'CRU_TS' in item_str:
+            print('avoid testing CRU_TS requiring credentials at ceda')
+        else:
             item = getattr(cat, item_str)
-            if isinstance(item, intake_xarray.netcdf.NetCDFSource) and item_str not in ['ocean.SOM_FFN', 'ocean.CSIR-ML6']:  # avoids too large (>500MB) datasets
+            if isinstance(item, (intake_xarray.NetCDFSource,intake_xarray.OpenDapSource)) and item_str not in ['ocean.SOM_FFN', 'ocean.CSIR-ML6']:  # avoids too large (>500MB) datasets
                 ds = item.to_dask()
                 assert isinstance(ds, xr.Dataset), print(item_str, 'could be accessed by to_dask() but isnt xr.Dataset')
                 print('successfully tested', item_str, '\n',type(item), '\n', ds.dims, '\n', ds.coords, '\nsize = ', format_bytes(ds.nbytes), '\n')
             elif isinstance(item, (intake_geopandas.RegionmaskSource, intake_geopandas.ShapefileSource)):
                 region = item.read()
                 print('successfully tested', item_str, '\n', type(item))
-            elif isinstance(item, intake_excel.ExcelSource):
+            elif isinstance(item, (intake.source.csv.CSVSource,intake_excel.ExcelSource)):
                 df = item.read()
                 print('successfully tested', item_str, '\n', type(df),'\n', df.head())
             else:
