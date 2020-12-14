7012d09fd8a0db35f892785ef46fe231c0656e31
Aaron Spring <aaronspring@users.noreply.github.com>
2020-12-14 14:32:13 +0100

    add cache_all_catalog_items to utils (#31)
    * add cache_all_catalog_items to utils

--- utils/convert.py
+++ utils/convert.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import xarray as xr
-    
+
 def monthly_csv_to_DataArray(df):
+    """Convert dataframes from NOAA catalog items to xr.DataArray.
+
+    Example:
+        >>> cat = intake.open_catalog('master.yaml')
+        >>> df = cat.climate.NOAA_correlation.read()
+        >>> da = monthly_csv_to_DataArray(df)
+        >>> da
+        <xr.DataArray> ...
+    """
     df=df.set_index('year')
     df = df.apply(pd.to_numeric,errors='coerce')
     initial = df.first_valid_index()
