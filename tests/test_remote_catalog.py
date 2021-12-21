978bbe64f2e6bf11e6a2f0b82d85d92c6f68a8b7
Aaron Spring <aaronspring@users.noreply.github.com>
2021-12-21 14:52:59 +0100

    Update test_remote_catalog.py

--- tests/test_remote_catalog.py
+++ tests/test_remote_catalog.py
@@ -64,9 +64,9 @@ def test_item(cat, item_str):
         elif isinstance(
             item,
             (
-            intake_geopandas.RegionmaskSource,
-            intake_geopandas.ShapefileSource,
-            intake_geopandas.GeoJSONSource
+                intake_geopandas.RegionmaskSource,
+                intake_geopandas.ShapefileSource,
+                intake_geopandas.GeoJSONSource
             ),
         ):
             region = item.read()
